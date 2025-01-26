00ae1be5d1095f4298414c70d55924f0786c8c03
Vyacheslav Lazarenko <vlazarenko@ptsecurity.com>
2025-01-26 16:19:49 +0300

    test_response_contains_all_fields

--- tests/tests_random_facts/test_random_facts.py
+++ tests/tests_random_facts/test_random_facts.py
@@ -16,6 +16,19 @@ class TestRandomFacts:
         with allure.step("Validate HTTP status code"):
             assert_that(status_code).is_equal_to(StatusCodes.HTTP_OK)
 
+    @allure.title("Test response body contains all expected fields")
+    def test_response_contains_all_fields(self, http_object):
+        with allure.step("Send GET request to /random endpoint"):
+            _, data = http_object.send_request(RequestTypes.GET, PATH, RandomFactsModel)
+        with allure.step("Validate response contains all expected fields"):
+            required_fields = {
+                field_name for field_name, field in RandomFactsModel.model_fields.items()
+                if not field.is_required
+            }
+            actual_fields = set(data.model_dump().keys())
+            missing_fields = required_fields - actual_fields
+            assert_that(missing_fields).is_empty()
+
     @allure.title("Test returns default type for request with default parameters")
     def test_default_parameters(self, http_object):
         with allure.step("Send GET request with default parameters"):
@@ -44,4 +57,3 @@ class TestRandomFacts:
         with allure.step("Validate datetime fields"):
             assert_that(data.createdAt).matches(r"\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z")
             assert_that(data.updatedAt).matches(r"\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z")
-
