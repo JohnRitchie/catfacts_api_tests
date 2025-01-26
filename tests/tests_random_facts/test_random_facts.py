265e06bea261175041bfb12bdaf8f00b69e1da57
Vyacheslav Lazarenko <vlazarenko@ptsecurity.com>
2025-01-26 13:37:44 +0300

    new tests

--- tests/tests_random_facts/test_random_facts.py
+++ tests/tests_random_facts/test_random_facts.py
@@ -9,3 +9,11 @@ class TestRandomFacts:
     def test_status_code_ok(self, http_object):
         status_code, _ = http_object.send_request(RequestTypes.GET, PATH, RandomFactsModel)
         assert_that(status_code).is_equal_to(StatusCodes.HTTP_OK)
+
+    def test_random_fact_default_parameters(self, http_object):
+        _, data = http_object.send_request(RequestTypes.GET, PATH, RandomFactsModel)
+        assert_that(data.type).is_equal_to('cat')
+
+    def test_random_fact_user_parameters(self, http_object):
+        _, data = http_object.send_request(RequestTypes.GET, PATH, RandomFactsModel, params={"animal_type": "horse"})
+        assert_that(data.type).is_equal_to('horse')
